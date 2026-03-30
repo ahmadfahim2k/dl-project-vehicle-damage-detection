fd74772a70d9e346e53357a5e0092ed294d2bf6a
ahmadfahim2k <ahmadfahim2k@gmail.com>
2026-03-30 16:47:08 +0100

    Updated UI

--- streamlit-app/app.py
+++ streamlit-app/app.py
@@ -1,6 +1,11 @@
 import streamlit as st
 from model_helper import predict
 
+st.set_page_config(layout="wide")
+
+with open("style.css") as f:
+    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
 st.title("Vehicle Damage Detection")
 
 uploaded_file = st.file_uploader("Upload the file", type=["jpg", "png"])
@@ -9,8 +14,16 @@ if uploaded_file:
     image_path = "temp_file.jpg"
     with open(image_path, "wb") as f:
         f.write(uploaded_file.getbuffer())
-        col1, col2, col3 = st.columns([1, 2, 1])
-        with col2:
-            st.image(uploaded_file, caption='Uploaded Image', use_container_width=True)
+
+    col1, col2 = st.columns([1, 1])
+    with col1:
+        st.image(uploaded_file, caption='Uploaded Image', use_container_width=True)
+    with col2:
         prediction = predict(image_path)
-        st.info(f'Predicted Class: {prediction}')
+        st.markdown(f"""
+            <div style="display: flex; align-items: center; justify-content: center; height: 100%;">
+                <p style="font-size: 3rem; font-weight: bold; text-align: center;">
+                    Predicted Class:<br>{prediction}
+                </p>
+            </div>
+        """, unsafe_allow_html=True)
